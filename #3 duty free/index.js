695b6b384e35d47d23895c389c28322e8a0bbc72
Benyamin Ahmed <benyaminahmed2@gmail.com>
2020-07-09 13:58:15 +0100

    Made web app that functions with the duty free code

--- #3 duty free/index.js
+++ #3 duty free/index.js
@@ -1,18 +1,15 @@
 const dutyFree = (normPrice, discount, hol) => {
-  const saving = (discount/100) * normPrice
+  const saving = (discount/100) * normPrice;
   
-  const bottlesPurchased = hol/saving
-  
-  return (Math.floor (bottlesPurchased) )
+  const bottlesPurchased = hol / saving;
+  return Math.floor(bottlesPurchased);
 }
-console.log(dutyFree(500, 10, 500));
-
-function onClickCalculate() {
+const onClickCalculate = () => {
   const normPrice = document.getElementById("normPrice").value;
   const discount = document.getElementById("discount").value;
   const holidayPrice = document.getElementById("holidayPrice").value;
 
-  const bottlesRequired = (normPrice, discount, holidayPrice);
+  const bottlesRequired = dutyFree(normPrice, discount, holidayPrice);
   
   document.getElementsByClassName("jumbotron")[0].style.display = 'block';
   document.getElementsByClassName("lead")[0].innerHTML = bottlesRequired;
